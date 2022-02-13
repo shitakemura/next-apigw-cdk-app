4f658e929abdf53a1455f83438ffadabd2c0769c
shitakemura <shitakemura@gmail.com>
2022-02-13 16:48:15 +0900

    Create snapshot test

--- packages/infra-backend/lib/infra-backend-stack.ts
+++ packages/infra-backend/lib/infra-backend-stack.ts
@@ -1,8 +1,15 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import * as cdk from "aws-cdk-lib";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import { Construct } from "constructs";
 
-export class InfraBackendStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class InfraBackendStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const todoTable = new dynamodb.Table(this, "TodoTable", {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+    });
   }
 }
---
+++ packages/infra-backend/test/infra-backend-snapshot.test.ts
@@ -0,0 +1,11 @@
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { InfraBackendStack } from "../lib/infra-backend-stack";
+
+test("snapshot test", () => {
+  const app = new cdk.App();
+  const stack = new InfraBackendStack(app, "InfraBackendTestStack");
+  const template = Template.fromStack(stack).toJSON();
+
+  expect(template).toMatchSnapshot();
+});
