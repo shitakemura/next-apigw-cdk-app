3a169e5d0b9573f1621a21b30fd43c1d873deb2c
shitakemura <shitakemura@gmail.com>
2022-02-15 11:53:21 +0900

    Add TodoBucketDeployment

--- packages/infra-backend/lib/frontend-deployment-stack.ts
+++ packages/infra-backend/lib/frontend-deployment-stack.ts
@@ -1,7 +1,9 @@
 import * as cdk from "aws-cdk-lib";
 import * as cloudFront from "aws-cdk-lib/aws-cloudfront";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3Deployment from "aws-cdk-lib/aws-s3-deployment";
 import { Construct } from "constructs";
+import * as path from "path";
 
 export class FrontendDeploymentStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -31,6 +33,17 @@ export class FrontendDeploymentStack extends cdk.Stack {
         ],
       }
     );
+
+    new s3Deployment.BucketDeployment(this, "TodoBucketDeployment", {
+      sources: [
+        s3Deployment.Source.asset(
+          path.resolve(__dirname, "../../frontend/out")
+        ),
+      ],
+      destinationBucket: todoBucket,
+      distribution: todoWebDestribution,
+      distributionPaths: ["/*"],
+    });
 
     new cdk.CfnOutput(this, "TodoWebDestributionName", {
       value: todoWebDestribution.distributionDomainName,
