d813ecb4875e815bd99ebc7b2f88053f726e73c4
shitakemura <shitakemura@gmail.com>
2022-02-17 17:48:21 +0900

    Introduce jest, msw, react testing library

---
+++ packages/frontend/mocks/handlers.ts
@@ -0,0 +1,26 @@
+import { rest, RestRequest } from "msw";
+import { Todo } from "../../shared/models";
+
+const testUserId = "testUserId";
+
+let todos: Todo[] = [
+  {
+    userId: testUserId,
+    id: "fdajlkow",
+    title: "First Todo",
+    completed: false,
+    createdAt: 1645085807054,
+  },
+];
+
+export const handlers = [
+  rest.get(
+    `${process.env.NEXT_PUBLIC_API_ENDPOINT_URL}/todos`,
+    (_, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json(todos.filter((todo) => todo.userId === testUserId))
+      );
+    }
+  ),
+];
